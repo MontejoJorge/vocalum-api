aa03d065d6abdc79543f47bc277c65fc3aba1e1b
MontejoJorge <jorgemon.lopez@gmail.com>
2022-10-06 16:31:48 +0200

    create ad model

--- database/migrations/2022_10_05_200034_create_users_table.php
+++ database/migrations/2022_10_05_200034_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->unique();
             $table->string('name');
             $table->string('surname');
             $table->integer('phone');
